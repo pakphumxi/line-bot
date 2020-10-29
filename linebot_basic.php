36c490bae335d9d0d3a0a7b64e75b4bd135bc834
pakphumxi <70052235+pakphumxi@users.noreply.github.com>
2020-10-29 16:25:38 +0700

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -14,8 +14,8 @@ foreach ($request_json['events'] as $event)
 		{
 			$text = $event['message']['text'];
 			
-			$reply_message = 'ฉันได้รับข้อความ '. $text.' ของคุณแล้ว!';   
-			
+			$reply_message = 'ฉันได้รับข้อความ '. $text.' ของคุณแล้ว!';
+			$reply_message = mySQL_selectAll('http://s61160189.kantit.com/json_select_users.php');
 		} else {
 			$reply_message = 'ฉันได้รับ '.$event['message']['type'].' ของคุณแล้ว!';
 		}
