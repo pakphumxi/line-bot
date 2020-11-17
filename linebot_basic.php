ddbacdf88ad83de49b56d5677e50820390543894
pakphumxi <70052235+pakphumxi@users.noreply.github.com>
2020-11-17 15:03:58 +0700

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -13,7 +13,7 @@ foreach ($request_json['events'] as $event)
 		if($event['message']['type'] == 'text')
 		{
 			
-			if($txts == "@บอท"){
+			if($event['message'] == "@บอท"){
 				
 				$reply_message = "กรุณาใช้รูปแบบคำสั่งที่ถูกต้องงงงง!!\n";
 				
