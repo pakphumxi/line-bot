3e4ad23f094f9f757eb36cc4e8b64f6fdee29a46
pakphumxi <70052235+pakphumxi@users.noreply.github.com>
2020-11-17 17:51:54 +0700

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -30,8 +30,8 @@ foreach ($request_json['events'] as $event)
 			if($arr[1] == "ฉันต้องการค้นหาข้อมูลนิสิตชื่อ"){
 				$datas = mySQL_selectAll('http://bot.kantit.com/json_select_users.php');
 				foreach($datas as $row){
-					if($row["user_firstname"] == $arr[3]){
-						$reply_message .= "พบชื่อ". $row["user_firstname"]." ". $row["user_lastname"];
+					if($row["user_firstname"] == "นาย".$arr[3] || $row["user_firstname"] == "นางสาว".$arr[3]){
+						$reply_message = "พบชื่อ". $row["user_firstname"]." ". $row["user_lastname"];
 					}
 				}
 			} 				
